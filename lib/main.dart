ea766d062fa1e4e2bedafc22a5979b7899103d7c
SAI KUMAR MISALA <saikumar@bluesapience.com>
2022-02-05 13:28:26 +0530

    app and search bar

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,7 @@ class MyApp extends StatelessWidget {
                 color: Colors.red,
                 size: 30.0,
               ),
-              tooltip: 'My Favorites',
+              tooltip: 'Favorites',
               onPressed: () {},
             ),
           ],
