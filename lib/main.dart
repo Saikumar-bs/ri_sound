4b85f1f1b73fc39fbe45df7c584652b7eb687996
SAI KUMAR MISALA <saikumar@bluesapience.com>
2022-02-05 21:30:13 +0530

    added singlechildscrollview and few other animals

--- lib/main.dart
+++ lib/main.dart
@@ -41,136 +41,242 @@ class MyApp extends StatelessWidget {
           ],
         ),
         body: SafeArea(
-          child: Column(
-            children: <Widget>[
-              Container(
-                alignment: Alignment.center,
-                margin: EdgeInsets.fromLTRB(10.0, 12.0, 10.0, 6.0),
-                padding: EdgeInsets.symmetric(horizontal: 20.0),
-                height: 45.0,
-                decoration: BoxDecoration(
-                    color: Colors.black12,
-                    borderRadius: BorderRadius.circular(20.0)),
-                child: TextField(
-                  decoration: InputDecoration(
-                    hintText: 'Search here',
-                    enabledBorder: InputBorder.none,
-                    focusedBorder: InputBorder.none,
+          child: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                Container(
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.fromLTRB(10.0, 12.0, 10.0, 6.0),
+                  padding: EdgeInsets.symmetric(horizontal: 20.0),
+                  height: 45.0,
+                  decoration: BoxDecoration(
+                      color: Colors.black12,
+                      borderRadius: BorderRadius.circular(20.0)),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Search here',
+                      enabledBorder: InputBorder.none,
+                      focusedBorder: InputBorder.none,
+                    ),
                   ),
                 ),
-              ),
-              Container(
-                margin: EdgeInsets.all(6.0),
-                padding: const EdgeInsets.all(6.0),
-                child: Column(
-                  children: <Widget>[
-                    Row(
-                      children: [
-                        Expanded(
-                          child: Column(
-                            children: <Widget>[
-                              Image.asset('images/antelope.png'),
-                              Container(
-                                padding: EdgeInsets.all(3.0),
-                                width: 200,
-                                alignment: Alignment.center,
-                                decoration: BoxDecoration(
-                                  border: Border.all(
-                                    color: Colors.black45,
+                Container(
+                  margin: EdgeInsets.all(6.0),
+                  padding: const EdgeInsets.all(6.0),
+                  child: Column(
+                    children: <Widget>[
+                      Row(
+                        children: [
+                          Expanded(
+                            child: Column(
+                              children: <Widget>[
+                                Image.asset('images/antelope.png'),
+                                Container(
+                                  padding: EdgeInsets.all(3.0),
+                                  width: 200,
+                                  alignment: Alignment.center,
+                                  decoration: BoxDecoration(
+                                    border: Border.all(
+                                      color: Colors.black45,
+                                    ),
+                                  ),
+                                  child: Text(
+                                    'Antelope',
+                                    style: TextStyle(
+                                      fontSize: 25.0,
+                                    ),
                                   ),
                                 ),
-                                child: Text(
-                                  'Antelope',
-                                  style: TextStyle(
-                                    fontSize: 25.0,
-                                  ),
-                                ),
-                              ),
-                            ],
+                              ],
+                            ),
                           ),
-                        ),
-                        SizedBox(width: 10.0),
-                        Expanded(
-                          child: Column(
-                            children: <Widget>[
-                              Image.asset('images/bee.png'),
-                              Container(
-                                padding: EdgeInsets.all(3.0),
-                                width: 200,
-                                alignment: Alignment.center,
-                                decoration: BoxDecoration(
-                                  border: Border.all(
-                                    color: Colors.black45,
+                          SizedBox(width: 10.0),
+                          Expanded(
+                            child: Column(
+                              children: <Widget>[
+                                Image.asset('images/bee.png'),
+                                Container(
+                                  padding: EdgeInsets.all(3.0),
+                                  width: 200,
+                                  alignment: Alignment.center,
+                                  decoration: BoxDecoration(
+                                    border: Border.all(
+                                      color: Colors.black45,
+                                    ),
+                                  ),
+                                  child: Text(
+                                    'Bee',
+                                    style: TextStyle(
+                                      fontSize: 25.0,
+                                    ),
                                   ),
                                 ),
-                                child: Text(
-                                  'Bee',
-                                  style: TextStyle(
-                                    fontSize: 25.0,
-                                  ),
-                                ),
-                              ),
-                            ],
+                              ],
+                            ),
                           ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 15.0),
-                    Row(
-                      children: [
-                        Expanded(
-                          child: Column(
-                            children: <Widget>[
-                              Image.asset('images/eagle.png'),
-                              Container(
-                                padding: EdgeInsets.all(3.0),
-                                width: 200,
-                                alignment: Alignment.center,
-                                decoration: BoxDecoration(
-                                  border: Border.all(
-                                    color: Colors.black45,
+                        ],
+                      ),
+                      SizedBox(height: 15.0),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: Column(
+                              children: <Widget>[
+                                Image.asset('images/eagle.png'),
+                                Container(
+                                  padding: EdgeInsets.all(3.0),
+                                  width: 200,
+                                  alignment: Alignment.center,
+                                  decoration: BoxDecoration(
+                                    border: Border.all(
+                                      color: Colors.black45,
+                                    ),
+                                  ),
+                                  child: Text(
+                                    'Eagle',
+                                    style: TextStyle(
+                                      fontSize: 25.0,
+                                    ),
                                   ),
                                 ),
-                                child: Text(
-                                  'Eagle',
-                                  style: TextStyle(
-                                    fontSize: 25.0,
-                                  ),
-                                ),
-                              ),
-                            ],
+                              ],
+                            ),
                           ),
-                        ),
-                        SizedBox(width: 10.0),
-                        Expanded(
-                          child: Column(
-                            children: <Widget>[
-                              Image.asset('images/elephant.png'),
-                              Container(
-                                padding: EdgeInsets.all(3.0),
-                                width: 200,
-                                alignment: Alignment.center,
-                                decoration: BoxDecoration(
-                                  border: Border.all(
-                                    color: Colors.black45,
+                          SizedBox(width: 10.0),
+                          Expanded(
+                            child: Column(
+                              children: <Widget>[
+                                Image.asset('images/elephant.png'),
+                                Container(
+                                  padding: EdgeInsets.all(3.0),
+                                  width: 200,
+                                  alignment: Alignment.center,
+                                  decoration: BoxDecoration(
+                                    border: Border.all(
+                                      color: Colors.black45,
+                                    ),
+                                  ),
+                                  child: Text(
+                                    'Elephant',
+                                    style: TextStyle(
+                                      fontSize: 25.0,
+                                    ),
                                   ),
                                 ),
-                                child: Text(
-                                  'Elephant',
-                                  style: TextStyle(
-                                    fontSize: 25.0,
-                                  ),
-                                ),
-                              ),
-                            ],
+                              ],
+                            ),
                           ),
-                        ),
-                      ],
-                    ),
-                  ],
+                        ],
+                      ),
+                      SizedBox(height: 15.0),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: Column(
+                              children: <Widget>[
+                                Image.asset('images/frog.png'),
+                                Container(
+                                  padding: EdgeInsets.all(3.0),
+                                  width: 200,
+                                  alignment: Alignment.center,
+                                  decoration: BoxDecoration(
+                                    border: Border.all(
+                                      color: Colors.black45,
+                                    ),
+                                  ),
+                                  child: Text(
+                                    'Frog',
+                                    style: TextStyle(
+                                      fontSize: 25.0,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          SizedBox(width: 10.0),
+                          Expanded(
+                            child: Column(
+                              children: <Widget>[
+                                Image.asset('images/macaw.png'),
+                                Container(
+                                  padding: EdgeInsets.all(3.0),
+                                  width: 200,
+                                  alignment: Alignment.center,
+                                  decoration: BoxDecoration(
+                                    border: Border.all(
+                                      color: Colors.black45,
+                                    ),
+                                  ),
+                                  child: Text(
+                                    'Macaw',
+                                    style: TextStyle(
+                                      fontSize: 25.0,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 15.0),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: Column(
+                              children: <Widget>[
+                                Image.asset('images/peacock.png'),
+                                Container(
+                                  padding: EdgeInsets.all(3.0),
+                                  width: 200,
+                                  alignment: Alignment.center,
+                                  decoration: BoxDecoration(
+                                    border: Border.all(
+                                      color: Colors.black45,
+                                    ),
+                                  ),
+                                  child: Text(
+                                    'Peacock',
+                                    style: TextStyle(
+                                      fontSize: 25.0,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          SizedBox(width: 10.0),
+                          Expanded(
+                            child: Column(
+                              children: <Widget>[
+                                Image.asset('images/toucan.png'),
+                                Container(
+                                  padding: EdgeInsets.all(3.0),
+                                  width: 200,
+                                  alignment: Alignment.center,
+                                  decoration: BoxDecoration(
+                                    border: Border.all(
+                                      color: Colors.black45,
+                                    ),
+                                  ),
+                                  child: Text(
+                                    'Toucan',
+                                    style: TextStyle(
+                                      fontSize: 25.0,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
